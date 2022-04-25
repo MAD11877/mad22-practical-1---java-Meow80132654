a609f55210379005d2f16a8f47d0134328f76868
Meow80132654 <57339761+Meow80132654@users.noreply.github.com>
2022-04-25 09:21:52 +0800

    BMI
    does the bmi

--- Question2.java
+++ Question2.java
@@ -9,8 +9,15 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
+    System.out.println("Prompts user for height and weight, calculate and print out the BMI of the user");
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
+    double weight = in.nextDouble();
+    double bmi = height/(weight*weight);
+    System.out.println("Your bmi is " + bmi);  
+    in.close();
+
+
+
   }
 }
